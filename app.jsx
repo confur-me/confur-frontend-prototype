503cbfb0e5527bae0987e8bab5055c9927f5980c
Shuvalov Anton <anton_shuvalov@me.com>
2015-05-06 01:03:24 +0300

    setup page.js & hello-world component

--- app.jsx
+++ app.jsx
@@ -1,20 +1,16 @@
 'use strict';
 
-console.log(123);
+import page from 'page';
+import React from 'react';
+import Hello from 'components/hello';
 
-// import page from 'page';
-// import React from 'react';
-// import Hello from 'components/hello';
-//
-// const root = document.getElementById('root');
-// React.render(<Hello />, root);
+const root = document.getElementById('root');
 
-// console.log(root);
-//
-// page('/', '/feed');
-//
-// page('/feed', (ctx, next) => {
-// });
-//
-// // Start router
-// page();
+page('/', '/feed');
+
+page('/feed', (ctx, next) => {
+  React.render(<Hello />, root);
+});
+
+// Start router
+page();
